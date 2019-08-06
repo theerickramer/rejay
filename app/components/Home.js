cf2f9b106baf6e4c92feb65fb6918054f497aefa
Eric Kramer <theerickramer@gmail.com>
2019-08-06 16:45:17 -0400

    init: parses folders

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,9 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
+import { remote } from 'electron';
+import fs from 'fs';
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
@@ -9,11 +11,52 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
+  constructor(props) {
+    super(props);
+    const { app } = remote;
+    const musicPath = `${app.getPath('music')}/iTunes/iTunes Media/Music`;
+    this.parseFolders(musicPath);
+  }
+
+  fileDetail = path => {
+    console.log(`FILE: ${path}`);
+  };
+
+  parseFolders = path => {
+    try {
+      fs.readdir(path, 'utf-8', (err, files) => {
+        if (err) {
+          if (String(err).match(/Error: ENOTDIR: not a directory/)) {
+            if (path.match(/\.aac|\.aif|\.mp3|\.wav/)) {
+              this.fileDetail(path);
+            }
+          }
+        } else {
+          files.forEach(file => {
+            this.parseFolders(`${path}/${file}`);
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  openDialog = () => {
+    const { dialog } = remote;
+    dialog.showOpenDialog(
+      {
+        properties: ['openFile', 'openDirectory', 'multiSelections']
+      },
+      files => console.log(files)
+    );
+  };
+
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+        {/* <div onClick={this.openDialog}>Choose Music Folder</div> */}
+        {/* <Link to={routes.COUNTER}>to Counter</Link> */}
       </div>
     );
   }
